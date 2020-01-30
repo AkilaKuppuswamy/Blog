329840960a05c5d2c7764bb0815684d009d8c286
AkilaKuppuswamy <akilainfomailbox@gmail.com>
2020-01-30 16:57:54 +0530

    added registration controller

--- src/main/java/technicalblog/controller/UserController.java
+++ src/main/java/technicalblog/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import technicalblog.model.Post;
 import technicalblog.model.User;
 import technicalblog.service.PostService;
+import technicalblog.service.UserService;
 
 import java.util.ArrayList;
 
@@ -17,6 +18,8 @@ public class UserController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("users/login")
     public String login() {
@@ -30,18 +33,25 @@ public class UserController {
 
     @RequestMapping(value = "users/login", method= RequestMethod.POST)
     public String loginUser(User user) {
-        return "redirect:/posts";
+        if(userService.login(user)) {
+            return "redirect:/posts";
+        }
+        else {
+            return "users/login";
+        }
     }
 
     @RequestMapping(value = "users/logout", method= RequestMethod.POST)
     public String logout(Model model) {
 
         ArrayList<Post> posts=postService.getAllPosts();
-
-
-
         model.addAttribute("posts",posts);
 
         return "index";
+    }
+
+    @RequestMapping(value = "users/registration", method=RequestMethod.POST)
+    public String registerUser(User user) {
+        return "users/login";
     }
 }
