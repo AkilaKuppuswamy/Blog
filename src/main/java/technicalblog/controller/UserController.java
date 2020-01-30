31feaefb182aebeb7d0e5951be1ab1e395650a79
AkilaKuppuswamy <akilainfomailbox@gmail.com>
2020-01-30 16:43:18 +0530

    once logout is pressed it returns to the index page and can find all post

--- src/main/java/technicalblog/controller/UserController.java
+++ src/main/java/technicalblog/controller/UserController.java
@@ -1,12 +1,22 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import technicalblog.model.Post;
 import technicalblog.model.User;
+import technicalblog.service.PostService;
+
+import java.util.ArrayList;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private PostService postService;
+
 
     @RequestMapping("users/login")
     public String login() {
@@ -24,7 +34,14 @@ public class UserController {
     }
 
     @RequestMapping(value = "users/logout", method= RequestMethod.POST)
-    public String logout() {
+    public String logout(Model model) {
+
+        ArrayList<Post> posts=postService.getAllPosts();
+
+
+
+        model.addAttribute("posts",posts);
+
         return "index";
     }
 }
